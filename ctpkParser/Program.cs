efea6f0512c56052856b1832d882c1ec8d44313a
Tobias Marstaller <tobias.marstaller@proton.me>
2026-04-27 16:01:24 +0200

    make sqlconverter callable from main entry point

--- ctpkParser/Program.cs
+++ ctpkParser/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ctpkLib;
 
 namespace ctpkParser
 {
@@ -12,8 +13,14 @@ namespace ctpkParser
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                SQLConverter.Main(args);
+                return;
+            }
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
